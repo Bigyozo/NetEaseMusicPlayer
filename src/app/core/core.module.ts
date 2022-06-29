464f4065e15b9468d5b0ee80ad9be192ff4d20d3
Bigyozo <502204620@qq.com>
2022-06-29 19:55:12 +0900

    add home page

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServicesModule } from '../services/services.module';
+import { PagesModule } from '../pages/pages.module';
+import { ShareModule } from '../share/share.module';
+import ja from '@angular/common/locales/ja';
+import { registerLocaleData } from '@angular/common';
+import { NZ_I18N, ja_JP } from 'ng-zorro-antd';
+
+registerLocaleData(ja);
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ServicesModule,
+    PagesModule,
+    ShareModule,
+    AppRoutingModule
+  ],
+  exports: [ShareModule, AppRoutingModule],
+  providers: [{ provide: NZ_I18N, useValue: ja_JP }]
+})
+export class CoreModule {
+  constructor(@SkipSelf() @Optional() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule can only be imported by AppModule');
+    }
+  }
+}
