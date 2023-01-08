c9aa67dd4a8163a5d5a05d5337749c2435c0af66
Bigyozo <502204620@qq.com>
2023-01-08 23:19:20 +0900

    centerPage

--- src/app/pages/member/center/center.component.ts
+++ src/app/pages/member/center/center.component.ts
@@ -1,9 +1,10 @@
 import { map } from 'rxjs/internal/operators';
+import { RecordVal, User, UserSheet } from 'src/app/services/data.types/member.type';
+import { SheetService } from 'src/app/services/sheet.service';
+import { BatchActionsService } from 'src/app/store/batch-actions.service';
 
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
-import { UserRecord, UserSheet } from '../../../services/data.types/member.type';
 
 @Component({
   selector: 'app-center',
@@ -11,13 +12,27 @@ import { UserRecord, UserSheet } from '../../../services/data.types/member.type'
   styleUrls: ['./center.component.less']
 })
 export class CenterComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {
+  user: User;
+  records: RecordVal[];
+  userSheet: UserSheet;
+
+  constructor(
+    private route: ActivatedRoute,
+    private sheetService: SheetService,
+    private batchActionsService: BatchActionsService
+  ) {
     this.route.data.pipe(map((res) => res.user)).subscribe(([user, userRecord, userSheet]) => {
-      console.log('user', user);
-      console.log('userRecord', userRecord);
-      console.log('userSheet', userSheet);
+      this.user = user;
+      this.records = userRecord;
+      this.userSheet = userSheet;
     });
   }
 
   ngOnInit() {}
+
+  onPlaySheet(id: number) {
+    this.sheetService.playsheet(id).subscribe((list) => {
+      this.batchActionsService.selectPlayList({ list, index: 0 });
+    });
+  }
 }
